1631bedda615ff02516c79d8a52c0a6e32ca218c
WYX <yukiwyx1019@gmail.com>
2020-07-21 11:01:30 +0900

    edit function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
+      flash[:success] = "ログインしました"
       redirect_to("/users/#{@user.id}")
     else
       flash[:danger] = "email/パスワードが間違っています。"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
-    if @user.save
+    if @user.update(user_params)
       session[:user_id] = @user.id
       flash[:success] = "ユーザー登録が完了しました"
       redirect_to("/users/#{@user.id}")
@@ -24,11 +24,11 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find_by(id: params[:id])
-    @user.name = params[:name]
-    @user.email = params[:email]
+    # @user.name = params[:name]
+    # @user.email = params[:email]
     # @user.password = params[:password]
-    if @user.save
-      # session[:user_id] = @user.id
+    if @user.update(user_params)
+      session[:user_id] = @user.id
       flash[:success] = "アカウント情報を更新しました"
       redirect_to("/users/#{@user.id}")
     else
