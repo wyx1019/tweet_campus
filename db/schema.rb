ab7d6809c19b2ab3c39cc694a7ee71340579d4a8
WYX <yukiwyx1019@gmail.com>
2020-07-27 17:53:00 +0900

    add admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_27_040455) do
+ActiveRecord::Schema.define(version: 2020_07_27_084751) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_07_27_040455) do
     t.string "password_digest"
     t.string "major"
     t.string "year"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name:  "Admin",
+             email: "admin@gmail.com",
+             password:              "000000",
+             password_confirmation: "000000",
+             admin: true)
