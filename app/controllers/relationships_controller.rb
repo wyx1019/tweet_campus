b5016d50f86032ac67a498a34eed8b83ede6b514
WYX <yukiwyx1019@gmail.com>
2020-07-29 17:03:11 +0900

    modify follow

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,7 +3,7 @@ class RelationshipsController < ApplicationController
     def create
         @user = User.find(params[:followed_id])
         @current_user.follow(@user)
-        redirect_to @user
+        redirect_back(fallback_location: root_path)
         # respond_to do |format|
         #     format.html { redirect_to @user }
         #     format.js
@@ -13,7 +13,7 @@ class RelationshipsController < ApplicationController
     def destroy
         @user = Relationship.find(params[:id]).followed
         @current_user.unfollow(@user)
-        redirect_to @user
+        redirect_back(fallback_location: root_path)
         # respond_to do |format|
         #     format.html { redirect_to @user }
         #     format.js
