17168ce2c7c5a884977cfdad02913277d0141417
WYX <yukiwyx1019@gmail.com>
2020-07-29 18:42:07 +0900

    complete follow ajax

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,20 +3,20 @@ class RelationshipsController < ApplicationController
     def create
         @user = User.find(params[:followed_id])
         @current_user.follow(@user)
-        redirect_back(fallback_location: root_path)
-        # respond_to do |format|
-        #     format.html { redirect_to @user }
-        #     format.js
-        #   end
+        # redirect_back(fallback_location: root_path)
+        respond_to do |format|
+            format.html { redirect_to @user }
+            format.js
+        end
     end
 
     def destroy
         @user = Relationship.find(params[:id]).followed
         @current_user.unfollow(@user)
-        redirect_back(fallback_location: root_path)
-        # respond_to do |format|
-        #     format.html { redirect_to @user }
-        #     format.js
-        #   end
+        # redirect_back(fallback_location: root_path)
+        respond_to do |format|
+            format.html { redirect_to @user }
+            format.js
+        end
     end
 end
