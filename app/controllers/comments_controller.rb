344c446dee8dc6b6b11d5ac92a5f28261c75fb49
WYX <yukiwyx1019@gmail.com>
2020-07-31 13:41:04 +0900

    complete comment show action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,8 @@
 class CommentsController < ApplicationController
+    before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+    before_action :not_login_user
+
+    def show
+        @comment = Comment.find(params[:id])
+    end
 end
