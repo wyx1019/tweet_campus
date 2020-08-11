13985bae0442c1e8f10675dc3a58517d14f503a5
WYX <yukiwyx1019@gmail.com>
2020-08-11 10:11:39 +0900

    JP

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,17 @@ module CampusTweets
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-
+    config.i18n.default_locale = :ja
     config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
+
+    config.generators do |g|
+      g.test_framework  false
+      g.stylesheets     false
+      g.javascripts     false
+      g.helper          false
+      g.channel         assets: false
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
