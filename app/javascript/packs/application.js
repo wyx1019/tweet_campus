8bbba1a01c51e4fa1ed2653a59a46062cb86f462
WYX <yukiwyx1019@gmail.com>
2020-08-12 12:25:23 +0900

    fix ajax problems

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,10 @@
-require("@rails/ujs").start();
-// require("turbolinks").start()
+// require("@rails/ujs").start();
+require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
+
+import Rails from "@rails/ujs";
+Rails.start();
 
 import "bootstrap";
 import "../stylesheets/application";
@@ -11,6 +14,9 @@ import "../javascripts/tab";
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
-//
+
 const images = require.context("../images", true);
 const imagePath = (name) => images(name, true);
+
+window.jQuery = $;
+window.$ = $;
