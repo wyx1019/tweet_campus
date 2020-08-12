d89317cc24b5d93774a76393695dbfb1e414119b
WYX <yukiwyx1019@gmail.com>
2020-08-12 17:26:49 +0900

    debug

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,7 +16,7 @@ import "../javascripts/tab";
 // or the `imagePath` JavaScript helper below.
 
 const images = require.context("../images", true);
-const imagePath = (name) => images(name, true);
+// const imagePath = (name) => images(name, true);
 
 window.jQuery = $;
 window.$ = $;
